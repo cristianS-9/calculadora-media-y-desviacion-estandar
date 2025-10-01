82d43c6a7afa59cc1b79cb344586b8f299240437
cristianS-9 <giovannyv292@gmail.com>
2025-10-01 16:49:52 -0500

    actualizacion de tipografia

--- index.php
+++ index.php
@@ -64,6 +64,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8" />
     <title>Calculadora Media y Desviación Estándar</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css" />
 </head>
 
